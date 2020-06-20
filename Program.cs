45668bd318807cdc28bc7f2c5cb74cd5198f29d4
cris391 <cristianpand@yahoo.com>
2020-06-20 15:45:12 +0200

    refactor main method

--- Program.cs
+++ Program.cs
@@ -3,39 +3,36 @@ using System.Collections.Generic;
 
 namespace sim_movement
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Console.Write("Please enter the table size: ");
-            // string tableSize = Console.ReadLine();
-            // List<int> tableSizelist = ToIntList(tableSize);
-            
-            // Console.Write("Please enter the object's starting position: ");
-            // string startingPostition = Console.ReadLine();
-            // List<int> startingPositionList = ToIntList(startingPostition);
+      Console.Write("Please enter the table size and the starting position of the object: ");
+      string tableSize = Console.ReadLine();
+      List<int> initializationParameters = ToIntList(tableSize);
+      RectangleTable rectangle = new RectangleTable();
+      rectangle.ComputeMatrix(initializationParameters[0], initializationParameters[1], initializationParameters[2], initializationParameters[3]);
 
-            RectangleTable rectangle = new RectangleTable("rectangle");
-            int[,] matrix = rectangle.computeMatrix(4, 4);
-            rectangle.visualizeMatrix(matrix);
-
-    
-
-        }
-
-        
-        private static List<int> ToIntList(string input)
-        {
-            string[] numbersAsStr = input.Split(",");
-            List<int> nums = new List<int>();
-            int oneNum;
-            foreach(string numberAsString in numbersAsStr)
-            {
-                if (Int32.TryParse(numberAsString, out oneNum))
-                    nums.Add(oneNum); 
-                    Console.WriteLine(oneNum);
-            }
-            return nums;
-        }
+      Console.Write("Please enter the commands: ");
+      string commandsStr = Console.ReadLine();
+      List<int> commands = ToIntList(commandsStr);
+      foreach (var command in commands)
+      {
+        rectangle.CommandObject(command);
+      }
     }
+
+    private static List<int> ToIntList(string input)
+    {
+      string[] numbersAsStr = input.Split(",");
+      List<int> nums = new List<int>();
+      int oneNum;
+      foreach (string numberAsString in numbersAsStr)
+      {
+        if (Int32.TryParse(numberAsString, out oneNum))
+          nums.Add(oneNum);
+      }
+      return nums;
+    }
+  }
 }
