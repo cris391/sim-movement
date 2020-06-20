910b239afc8d18454cbd9a6f4e7906113f25ea78
cris391 <cristianpand@yahoo.com>
2020-06-20 15:46:01 +0200

    rectable implementation v1

--- Shapes/RectangleTable.cs
+++ Shapes/RectangleTable.cs
@@ -1,58 +1,162 @@
 using System;
 
-public class RectangleTable : Shape
+public class RectangleTable : BasicTable
 {
-  private string name;
-  private int width;
-  private int height;
-  private int[] currentObjectPosition = {0, 0};
+  private int[,] matrix;
 
-  public RectangleTable(string name)
+  public RectangleTable(){}
+
+  public void ComputeMatrix(int width, int height, int x, int y)
   {
-    this.name = name;
+    matrix = new int[width, height];
+
+    currentObjectPosition[0] = x;
+    currentObjectPosition[1] = y;
+    matrix[x, y] = 1;
   }
 
-
-  public override void setSize(int width, int height)
+  // maybe we should decompose this so its easier to extend functionality such as rotating the table instead of the object
+  public override void CommandObject(int command)
   {
-    this.width = width;
-    this.height = height;
+    switch (command)
+    {
+      case 0:
+        // Console.WriteLine("Quitting simulation");
+        Console.WriteLine("[{0}, {1}]", currentObjectPosition[1], currentObjectPosition[0]);
+        Environment.Exit(1);
+        break;
+      case 1:
+        // Console.WriteLine("Current object direction: " + (Direction)currentObjectDirection);
+        try
+        {
+          moveForward();
+        }
+        catch (IndexOutOfRangeException)
+        {
+          Console.WriteLine("[-1, -1]");
+          Environment.Exit(1);
+        }
+        break;
+      case 2:
+        // Console.WriteLine("Current object direction: " + (Direction)currentObjectDirection);
+        try
+        {
+          moveBackward();
+        }
+        catch (IndexOutOfRangeException)
+        {
+          Console.WriteLine("[-1, -1]");
+          Environment.Exit(1);
+        }
+        break;
+      case 3:
+        // Console.WriteLine("Rotating clockwise 90 degrees");
+        if (currentObjectDirection < 3)
+        {
+          currentObjectDirection++;
+        }
+        else
+        {
+          currentObjectDirection = 0;
+        }
+        // Console.WriteLine("Current object direction: " + (Direction)currentObjectDirection);
+        break;
+      case 4:
+        if (currentObjectDirection > 0)
+        {
+          currentObjectDirection--;
+        }
+        else
+        {
+          currentObjectDirection = 3;
+        }
+        // Console.WriteLine("Rotating counterclockwise 90 degrees");
+        // Console.WriteLine("Current object direction: " + (Direction)currentObjectDirection);
+        break;
+      default:
+        Console.WriteLine("Default case");
+        break;
+    }
   }
 
-  public override int[] getSize()
+  private void moveForward()
   {
-    return new int[] { width, height };
+    // visualizeMatrix();
+    // Console.WriteLine("Moving forward 1 step");
+    if (currentObjectDirection == 0)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[--currentObjectPosition[0], currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 1)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[currentObjectPosition[0], ++currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 2)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[++currentObjectPosition[0], currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 3)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[currentObjectPosition[0], --currentObjectPosition[1]] = 1;
+    }
+    // Console.WriteLine("-----------");
+    // visualizeMatrix();
   }
 
-  public override int[,] computeMatrix(int width, int height)
+  private void moveBackward()
   {
-    int[,] matrix = new int[4, 4];
-    // int[,] matrix = new int[4, 4]{ { 1, 2, 3, 4 }, 
-    //                                 { 5, 6, 7, 8 }, 
-    //                                 { 9, 10, 11, 12 }, 
-    //                                 { 13, 14, 15, 16 }
-    //                                 };
-    
-    return matrix;
+    // visualizeMatrix();
+    // Console.WriteLine("Moving backward 1 step");
+    if (currentObjectDirection == 0)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[++currentObjectPosition[0], currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 1)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[currentObjectPosition[0], --currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 2)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[--currentObjectPosition[0], currentObjectPosition[1]] = 1;
+    }
+    else if (currentObjectDirection == 3)
+    {
+      matrix[currentObjectPosition[0], currentObjectPosition[1]] = 0;
+      matrix[currentObjectPosition[0], ++currentObjectPosition[1]] = 1;
+    }
+    // Console.WriteLine("-----------");
+    // visualizeMatrix();
   }
 
-  public void visualizeMatrix(int [,] matrix)
+  public override void VisualizeMatrix()
   {
     int rowLength = matrix.GetLength(0);
     int colLength = matrix.GetLength(1);
-        for (int i = 0; i < rowLength; i++)
-        {
-            for (int j = 0; j < colLength; j++)
-            {
-                Console.Write(string.Format("{0} ", matrix[i, j]));
-                // Console.Write(string.Format("{0} ", matrix[2, 1]));
-            }
-            Console.Write(Environment.NewLine + Environment.NewLine);
-        }
+    for (int i = 0; i < rowLength; i++)
+    {
+      for (int j = 0; j < colLength; j++)
+      {
+        Console.Write(string.Format("{0} ", matrix[i, j]));
+        // Console.Write(string.Format("{0} ", matrix[2, 1]));
+      }
+      Console.Write(Environment.NewLine + Environment.NewLine);
+    }
   }
 
-  public override void commandObject(int command)
+  public override void RotateTable(int direction)
   {
+    throw new NotImplementedException();
+  }
 
+  public override void MoveObject(int direction)
+  {
+    throw new NotImplementedException();
   }
 }
